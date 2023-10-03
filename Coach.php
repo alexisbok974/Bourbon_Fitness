157cba5ecc7c5797fdff3779229898ffdd7a7eaa
tra_GH//974 <theoriv97480@gmail.com>
2023-10-03 20:37:41 +0200

    commit 3

--- Coach.php
+++ Coach.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste des Coachs</title>
     <link rel="stylesheet" href="StyleCoach.css">
 </head>
@@ -14,54 +16,58 @@
 </div>
 
 <body>
-    <h1>Liste des Coachs</h1>
-    <table border="1">
-        <tr>
-            <th>ID</th>
-            <th>Nom</th>
-            <th>Prénom</th>
-            <th>Sexe</th>
-            <th>Spécialité</th>
-            <th>Informations</th>
-        </tr>
-        <?php
-        // Configuration de la connexion à la base de données
-        $serveur = "localhost";
-        $utilisateur = "root";
-        $motDePasse = "root";
-        $baseDeDonnees = "BourbonFitness";
 
-        // Connexion à la base de données
-        $connexion = new mysqli($serveur, $utilisateur, $motDePasse, $baseDeDonnees);
+    
+    <div class="tableau">
+        <h1>Liste des Coachs</h1>
+        <table border="1" class="styleTab">
+            <tr>
+                <th>ID</th>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Sexe</th>
+                <th>Spécialité</th>
+                <th>Informations</th>
+            </tr>
+            <?php
+            // Configuration de la connexion à la base de données
+            $serveur = "localhost";
+            $utilisateur = "root";
+            $motDePasse = "root";
+            $baseDeDonnees = "BourbonFitness";
 
-        // Vérification de la connexion
-        if ($connexion->connect_error) {
-            die("Échec de la connexion à la base de données : " . $connexion->connect_error);
-        }
+            // Connexion à la base de données
+            $connexion = new mysqli($serveur, $utilisateur, $motDePasse, $baseDeDonnees);
 
-        // Requête SQL pour récupérer les utilisateurs
-        $requete = "SELECT idCoach, nomCoach, prenomCoach, sexeCoach, specialite, emailCoach, telephoneCoach FROM Coach";
-        $resultat = $connexion->query($requete);
-
-        // Affichage des utilisateurs dans le tableau HTML
-        if ($resultat->num_rows > 0) {
-            while ($row = $resultat->fetch_assoc()) {
-                echo "<tr>";
-                echo "<td>" . $row["idCoach"] . "</td>";
-                echo "<td>" . $row["nomCoach"] . "</td>";
-                echo "<td>" . $row["prenomCoach"] . "</td>";
-                echo "<td>" . $row["sexeCoach"] . "</td>";
-                echo "<td>" . $row["specialite"] . "</td>";
-                echo '<td><a href="DetailsCoach.php?idCoach=' . $row["idCoach"] . '">Voir les détails</a></td>';
-                echo "</tr>";
+            // Vérification de la connexion
+            if ($connexion->connect_error) {
+                die("Échec de la connexion à la base de données : " . $connexion->connect_error);
             }
-        } else {
-            echo "Aucun utilisateur trouvé.";
-        }
 
-        // Fermeture de la connexion à la base de données
-        $connexion->close();
-        ?>
-    </table>
+            // Requête SQL pour récupérer les utilisateurs
+            $requete = "SELECT idCoach, nomCoach, prenomCoach, sexeCoach, specialite, emailCoach, telephoneCoach FROM Coach";
+            $resultat = $connexion->query($requete);
+
+            // Affichage des utilisateurs dans le tableau HTML
+            if ($resultat->num_rows > 0) {
+                while ($row = $resultat->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row["idCoach"] . "</td>";
+                    echo "<td>" . $row["nomCoach"] . "</td>";
+                    echo "<td>" . $row["prenomCoach"] . "</td>";
+                    echo "<td>" . $row["sexeCoach"] . "</td>";
+                    echo "<td>" . $row["specialite"] . "</td>";
+                    echo '<td><a href="DetailsCoach.php?idCoach=' . $row["idCoach"] . '">Voir les détails</a></td>';
+                    echo "</tr>";
+                }
+            } else {
+                echo "Aucun utilisateur trouvé.";
+            }
+
+            // Fermeture de la connexion à la base de données
+            $connexion->close();
+            ?>
+        </table>
+    </div>
 </body>
 </html>
